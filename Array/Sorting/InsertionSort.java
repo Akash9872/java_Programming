9f02871299acc0f0557e5c7966bb33bc6a89495c
Akash9872 <153419206+Akash9872@users.noreply.github.com>
2026-04-20 12:13:39 +0530

    Add InsertionSort program with package declaration

---
+++ Array/Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        System.out.println("Starting Insertion Sort...");
+        int[] arr = {4,1,7,3,9,2,0,8};
+
+        for(int i = 1; i < arr.length; i++){
+            int j = i;
+
+            while(j > 0 && arr[j] < arr[j - 1]){
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+
+        System.out.println("Sorted Array: ");
+        for(int ele : arr) 
+            System.out.print(ele + " ");
+        System.out.println();
+    }
+}
